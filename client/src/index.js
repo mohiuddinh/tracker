d615bc380b1c57a750eaa3daf30fd53cee2628e8
Hassan Mohiuddin <hassan_m@mit.edu>
2021-07-15 21:07:07 -0400

    Reverted commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,34 +1,36 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'core-js';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-
-import App from './App';
 import './index.css';
-import rootReducer from './store/reducers';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from "react-router-dom";
 
-const initialState = {};
+import Reducer from './_reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(thunk),
-    (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
-      compose,
-  ),
-);
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </Router>
-  </Provider>,
-  document.getElementById('root'),
-);
+    <Provider
+        store={createStoreWithMiddleware(
+            Reducer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+    >
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- server/src/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import usersRoutes from './users';
-import messagesRoutes from './messages';
-const router = Router();
-
-router.use('/users', usersRoutes);
-router.use('/messages', messagesRoutes);
-
-export default router;
