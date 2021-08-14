4c0d64602a2c313c8856874d98804b963ee906b3
Hassan Mohiuddin <hassan_m@mit.edu>
2021-08-14 18:33:27 -0400

    can pass in gamertag and console now

--- client/src/components/views/GameTile/ApexTile.js
+++ client/src/components/views/GameTile/ApexTile.js
@@ -1,95 +1,92 @@
 import { urlencoded } from "body-parser";
 import React, { useState } from "react";
-import './GameTile.css';
+import "./GameTile.css";
 const axios = require("axios").default;
 
-
-
 function ApexTile() {
+  const [game, setGame] = useState({
+    username: "",
+    level: "",
+    battlepass: "",
+    rank: "",
+    rankDiv: "",
+    recentLegend: "",
+    chosenLegend: "",
+    arenaRank: "",
+    arenaRankDiv: "",
+    currentLegendIcon: "",
+    rankScore: "",
+    chosenLegend: "",
+  });
 
-
-    const [game, setGame] = useState({
-        username: "",
-        level: "",
-        battlepass: "",
-        rank: "",
-        rankDiv: "",
-        recentLegend: "",
-        chosenLegend: "",
-        arenaRank: "",
-        arenaRankDiv: "",
-        currentLegendIcon: "",
-        rankScore: "",
-        chosenLegend: "",
-
+  function callAPI() {
+    let variable = { gamertag: "Kombatkid13", console: "X1" };
+    axios.post("/api/apex/test", variable).then((res) => {
+        console.log(res); 
+      setGame((prev) => {
+        return {
+          ...prev,
+          username: res.data.data.global.name,
+          level: res.data.data.global.level,
+          battlepass: res.data.data.global.battlepass.level,
+          rank: res.data.data.global.rank.rankName,
+          rankScore: res.data.data.global.rank.rankScore,
+          rankDiv: res.data.data.global.rank.rankDiv,
+          recentLegend: res.data.data.realtime.selectedLegend,
+          arenaRank: res.data.data.global.arena.rankName,
+          arenaRankDiv: res.data.data.global.arena.rankDiv,
+          currentLegendIcon: res.data.data.legends.selected.ImgAssets.icon,
+        };
+      });
     });
+  }
 
-    function callAPI() {
+  return (
+    <div className="GameTile">
+      <h1>Apex Legends</h1>
 
-    axios.get("/api/apex/test").then((res) => {
-    
-        setGame((prev) => {
-            return ({
-            ...prev,
-            username: res.data.data.global.name,
-            level: res.data.data.global.level,
-            battlepass: res.data.data.global.battlepass.level,
-            rank: res.data.data.global.rank.rankName,
-            rankScore: res.data.data.global.rank.rankScore,
-            rankDiv: res.data.data.global.rank.rankDiv,
-            recentLegend: res.data.data.realtime.selectedLegend,
-            arenaRank: res.data.data.global.arena.rankName,
-            arenaRankDiv: res.data.data.global.arena.rankDiv,
-            currentLegendIcon: res.data.data.legends.selected.ImgAssets.icon,
-            })
-        })
-        });
-    }
+      <div className="Stats">
+        <h2 className="StatTile Row">Username</h2>
+        <p className="Stat Row">{game.username}</p>
+      </div>
+      <hr></hr>
+      <div className="Stats">
+        <h2 className="StatTile Row">Current Level</h2>
+        <p className="Stat Row">{game.level}</p>
+      </div>
+      <hr></hr>
+      <div className="Stats">
+        <h2 className="StatTile Row">Current Battlepass Level</h2>
+        <p className="Stat Row">{game.battlepass}</p>
+      </div>
+      <hr></hr>
+      <div className="Stats">
+        <h2 className="StatTile Row">Current BR Rank</h2>
+        <p className="Stat Row">
+          {game.rank} {game.rankDiv}
+        </p>
+      </div>
+      <hr></hr>
+      <div className="Stats">
+        <h2 className="StatTile Row">Current BR Rank Score</h2>
+        <p className="Stat Row">{game.rankScore}</p>
+      </div>
+      <hr></hr>
+      <div className="Stats">
+        <h2 className="StatTile Row">Most Recently Played Legend</h2>
+        <p className="Stat Row">{game.recentLegend}</p>
+      </div>
+      <hr></hr>
+      <div className="Stats">
+        <h2 className="StatTile Row">Current Arena Rank</h2>
+        <p className="Stat Row">
+          {game.arenaRank} {game.arenaRankDiv}
+        </p>
+      </div>
 
-
-    return (
-        <div className="GameTile">
-            <h1>Apex Legends</h1>
-
-            <div className="Stats">
-                <h2 className="StatTile Row">Username</h2>
-                <p className="Stat Row">{game.username}</p>
-                </div>
-                <hr></hr>
-                <div className="Stats">
-                <h2 className="StatTile Row">Current Level</h2>
-                <p className="Stat Row">{game.level}</p>
-                </div>
-                <hr></hr>
-                <div className="Stats">
-                <h2 className="StatTile Row">Current Battlepass Level</h2>
-                <p className="Stat Row">{game.battlepass}</p>
-                </div>
-                <hr></hr>
-                <div className="Stats">
-                <h2 className="StatTile Row">Current BR Rank</h2>
-                <p className="Stat Row">{game.rank} {game.rankDiv}</p>
-                </div>
-                <hr></hr>
-                <div className="Stats">
-                <h2 className="StatTile Row">Current BR Rank Score</h2>
-                <p className="Stat Row">{game.rankScore}</p>
-                </div>
-                <hr></hr>
-                <div className="Stats">
-                <h2 className="StatTile Row">Most Recently Played Legend</h2>
-                <p className="Stat Row">{game.recentLegend}</p>
-                </div>
-                <hr></hr>
-                <div className="Stats">
-                <h2 className="StatTile Row">Current Arena Rank</h2>
-                <p className="Stat Row">{game.arenaRank} {game.arenaRankDiv}</p>
-                </div>
-
-                <button onClick={callAPI}>Click Here</button>
-        </div>
-    )
-
+      <button onClick={callAPI}>Click Here</button>
+    </div>
+  );
 }
 
 export default ApexTile;
--- server/routes/apex.js
+++ server/routes/apex.js
@@ -4,11 +4,17 @@ const router = express.Router();
 const { auth } = require("../middleware/auth");
 var axios = require("axios");
 
-router.get("/test", auth, (req, res) => {
-  console.log("yeee boi");
+router.post("/test", auth, (req, res) => {
+  console.log(req.body); 
+  let thisURL =
+    "https://apex-legends.p.rapidapi.com/stats/" +
+    req.body.gamertag +
+    "/" +
+    req.body.console;
+
   var apexOptions = {
     method: "GET",
-    url: "https://apex-legends.p.rapidapi.com/stats/Kombatkid13/X1",
+    url: thisURL,
     headers: {
       "x-rapidapi-key": "f4a936d572msh54c9a38579e5eedp1223c7jsnf3bf55cba7f7",
       "x-rapidapi-host": "apex-legends.p.rapidapi.com",
@@ -27,4 +33,4 @@ router.get("/test", auth, (req, res) => {
     });
 });
 
-module.exports = router; 
+module.exports = router;
